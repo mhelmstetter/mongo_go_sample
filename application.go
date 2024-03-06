b6ae861106c548c77649adbf94497193b9adfc67
Frank Snow <fsnow@fsnow.com>
2024-03-06 12:21:00 -0500

    Add health check message at /

--- application.go
+++ application.go
@@ -48,6 +48,7 @@ func init() {
 
 func main() {
 	r := mux.NewRouter()
+	r.HandleFunc("/", healthCheck).Methods("GET")
 	r.HandleFunc("/upsert", upsertDocument).Methods("GET")
 	r.HandleFunc("/find", findDocuments).Methods("GET")
 
@@ -108,4 +109,8 @@ func findDocuments(w http.ResponseWriter, r *http.Request) {
 
 	// Return count
 	fmt.Fprintf(w, "Number of documents found: %d\n", count)
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "All good here at %s\n", time.Now().String())
 }
